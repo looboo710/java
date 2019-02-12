6d60b23422b8987b73e73f803dd4e0c5a09a0979
looboo710 <43814887+looboo710@users.noreply.github.com>
2019-02-12 07:28:36 -0500

    Update ExerciseTwo.java

--- Lab02/src/Lab02/ExerciseTwo.java
+++ Lab02/src/Lab02/ExerciseTwo.java
@@ -1,10 +1,4 @@
-/*
- * Name: Josh Yoon
- * Student ID: 040 939 635
- * Course & Section: CST8132 304
- * Assignment: Lab 2
- * Date: Jan 20, 2019
- */
+
 package Lab02;
 
 public class ExerciseTwo {
